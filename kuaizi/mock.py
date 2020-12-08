bd4da3bcf806676fa94ee5b62306d18f0b24fc45
Jiaxuan Li <jiaxuan_li@pku.edu.cn>
2020-12-09 00:18:48 +0800

    improve moock

--- kuaizi/mock.py
+++ kuaizi/mock.py
@@ -29,9 +29,10 @@ class Data:
     While it is possible for scarlet to run without wcs or psf,
     it is strongly recommended not to, which is why these entry are not optional.
     """
-    def __init__(self, images, variances, masks, channels, wcs, psfs=None, info=None):
+    def __init__(self, images, variances, masks, channels, wcs, weights=None, psfs=None, info=None):
         self.images = images
         self.variances = variances
+        self.weights = weights # weights and variances don't necessarily need to be the same.
         self.masks = masks
         self.channels = channels
         self.wcs = wcs
@@ -52,6 +53,13 @@ class Data:
     @variances.setter
     def variances(self, variances):
         self._variances = variances
+
+    @property
+    def weights(self):
+        return self._weights
+    @weights.setter
+    def weights(self, weights):
+        self._weights = weights
 
     @property
     def masks(self):
@@ -202,6 +210,7 @@ class MockGal:
                                                    flux=knot_frac, 
                                                    rng=rng)
                     gal = galsim.Add([gal, knots])
+                    print(gal.flux)
                 
                 gal = gal.withFlux(tot_flux * galaxy['flux_fraction'][k]) # Get Flux
                 gal_list.append(gal)
@@ -366,6 +375,79 @@ class MockGal:
             raise ValueError('Other formats are not supported yet. Please use `pkl`.')
             return
 
+
+
+"""
+
+# Determine the coefficient when converting image flux to `sigma map` for HSC
+def calc_sigma_coeff(images, variance_map):
+    '''
+    This function empirically calculate the coefficient used to convert `image flux` to `sigma map`.
+    This only works for single band now. 
+    
+    `sigma map = coeff * sqrt(image_flux)`, where `sigma map = sqrt(variance map)`. 
+
+    Parameters:
+        images (numpy 2-D array or 3-D array for multiple bands): image array. Band is along the first axis.
+        variance_map (numpy 2-D array or 3-D array for multiple bands): variance array, 
+            i.e., the third layer of HSC cutout (`hdu[3].data`).
+
+    Return:
+        A_best (numpy array): coeff in each band
+    '''
+    from kuaizi.utils import extract_obj
+    from astropy.convolution import convolve, Gaussian2DKernel
+
+    if len(images.shape) == 2: # single band
+        images = images[np.newaxis, :, :]
+        variance_map = variance_map[np.newaxis, :, :]
+
+    sigma_map = np.sqrt(variance_map)
+    # Generate a mask, which only includes bright objects
+    obj_cat, segmap = extract_obj(images.mean(axis=0), b=32, f=2, sigma=3, show_fig=False, verbose=False)
+    mask = segmap > 0
+    mask = convolve(mask, Gaussian2DKernel(2)) > 0.2
+    ### Calculate the loss between `sigma_map` and `images`. `A` is a coefficient.
+    sigma_map -= np.median(sigma_map, axis=(1, 2))[:, np.newaxis, np.newaxis] # remove a background
+    mask = np.repeat(mask[np.newaxis, :, :], len(images), axis=0) # len(images) is the number of bands
+    
+    A_set = np.linspace(0, 0.01, 200)
+    loss = np.zeros([len(images), len(A_set)])
+    for i, A in enumerate(A_set):
+        temp = np.sqrt(images)
+        temp[np.isnan(temp)] = 0.0
+        diff = (sigma_map - A * temp)[mask].reshape(len(images), -1)
+        # diff = diff[~np.isnan(diff)]
+        loss[:, i] = np.linalg.norm(diff, axis=1) / np.sum(mask[0])
+    A_best = A_set[np.argmin(loss, axis=1)]
+
+    return A_best
+
+def mock_variance(mock_img, images, variance):
+    '''
+    Generate mock variance map based on given mock image. 
+    `Band` is always along the first axis of the array.
+    E.g., `images.shape = (4, 300, 300)` corresponds to images in `g, r, i, z` bands.
+
+    Parameters:
+        mock_img (numpy array): array for the image of mock galaxy. 
+        images (numpy array): array for the original image (onto which the mock galaxy is placed).
+        variance (numpy array): array for the original variance map (`hdu[3].data` for HSC).
+
+    Returns:
+        mock_variance (numpy array): array for the mock variance.
+
+    '''
+    A = calc_sigma_coeff(images, variance)
+    sigma_map = np.sqrt(variance)
+    temp = np.sqrt(mock_img)
+    temp[np.isnan(temp)] = 0.0
+    mock_sigma = sigma_map + A[:, np.newaxis, np.newaxis] * temp
+    mock_variance = mock_sigma ** 2
+    return mock_variance
+
+
+"""
 
 # Determine the coefficient when converting image flux to `sigma map` for HSC
 def calc_sigma_coeff(images, variance_map):
