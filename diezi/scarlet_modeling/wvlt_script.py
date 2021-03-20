f56985cd251e597e6a255fa93611e5461c5e1e9b
Jiaxuan Li <jiaxuanl@tigercpu.princeton.edu>
2021-03-20 10:20:18 -0400

    first job on slurm

--- diezi/scarlet_modeling/wvlt_script.py
+++ diezi/scarlet_modeling/wvlt_script.py
@@ -1,14 +1,12 @@
 import fire
 import os
+import kuaizi as kz
 from kuaizi.fitting import fitting_wavelet_obs_tigress
 
 from astropy.io import fits
 from astropy.table import Table
 
-kz.utils.set_env(project='HSC', name='LSBG', data_dir='/tigress/jiaxuanl/Data')
-
-lsbg_cat = Table.read('./Cutout/Candy/candy_cutout_cat.fits')
-
+lsbg_cat = Table.read('/tigress/jiaxuanl/Data/HSC/LSBG/Cutout/Candy/candy_cutout_cat.fits')
 
 def run_scarlet_wvlt(index):
     blend = fitting_wavelet_obs_tigress(
