fix a bug about units

diezi/gen_cutout/tiger_cutout_hsc.py
@@ -225,7 +225,7 @@ def prepare_catalog(cat, size, ra='ra', dec='dec', name=None, unit='arcsec',
         # Using the same size for all objects
         size_arr = np.full(len(cat), float(size))
     else:
-        size_arr = cat[size]
+        size_arr = cat[size].data
 
     # Add size unit if necessary
     if unit != 'pixel':
@@ -375,8 +375,7 @@ def generate_cutout(butler, skymap, ra, dec, band='i', label='deepCoadd_skyMap',
 
 def cutout_one(butler, skymap, obj, band, label, psf):
     """Generate cutout for a single object."""
-    prefix, ra, dec, radius = obj['prefix'], obj['ra'], obj['dec'], obj['radius'] * \
-        u.arcmin  # might be bugy
+    prefix, ra, dec, radius = obj['prefix'], obj['ra'], obj['dec'], obj['radius']
     prefix = '_'.join([prefix, band.lower().strip()])
 
     try: