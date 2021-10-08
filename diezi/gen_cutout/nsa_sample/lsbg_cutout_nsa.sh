0150946987be431efa0a0504d4f150132df5920b
Jiaxuan Li <jiaxuanl@princeton.edu>
2021-10-07 20:29:55 -0400

    add another 200 objects as test sample

--- diezi/gen_cutout/nsa_sample/lsbg_cutout_nsa.sh
+++ diezi/gen_cutout/nsa_sample/lsbg_cutout_nsa.sh
@@ -9,8 +9,8 @@ export LSBGDIR="/scratch/gpfs/jiaxuanl/Data/HSC/LSBG"
 # cat['radius'] is always the cutout size we should use!!!!
 python3 ../s18a_batch_cutout.py \
     $DATADIR\
-    $LSBGDIR"/Catalog/nsa_test_sample_210928.fits" \
+    $LSBGDIR"/Catalog/nsa_test_sample_211007.fits" \
     --bands grizy --ra_name ra --dec_name dec \
     --name "viz-id" --output $LSBGDIR"/Cutout/NSA" \
     --size "cutout_size" --prefix "nsa" \
-    --njobs 2 --psf
+    --njobs 3 --psf
