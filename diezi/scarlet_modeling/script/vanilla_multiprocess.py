c94aab25686c3f972d9fd620e34654d84fe6db0d
Jiaxuan Li <jiaxuanl@princeton.edu>
2022-01-06 01:22:54 -0500

    move vanilla multiprocess code to script folder

---
+++ diezi/scarlet_modeling/script/vanilla_multiprocess.py
@@ -0,0 +1,64 @@
+import fire
+import os
+import numpy as np
+import kuaizi as kz
+from kuaizi.fit import fitting_obs_tigress
+
+from multiprocessing import Pool, Manager
+from functools import partial
+
+from astropy.table import Table
+
+
+def run_scarlet_vanilla(index, cat, DATADIR, OUTPUT_DIR, PREFIX, starlet_thresh=0.5, global_logger=None, fail_logger=None):
+    blend = fitting_obs_tigress(
+        {'project': 'HSC', 'name': 'LSBG', 'data_dir': DATADIR},
+        cat[index],
+        name='viz-id',
+        method='vanilla',
+        channels='griz',
+        starlet_thresh=starlet_thresh,
+        prefix=PREFIX.lower(),
+        figure_dir=os.path.join(OUTPUT_DIR, f'Figure/{PREFIX.lower()}/'),
+        model_dir=os.path.join(OUTPUT_DIR, f'Model/{PREFIX.lower()}/'),
+        log_dir=os.path.join(OUTPUT_DIR, f'log/{PREFIX.lower()}/'),
+        show_figure=False,
+        global_logger=global_logger,
+        fail_logger=fail_logger)
+    del blend
+    return
+
+
+def multiprocess_fitting(DATADIR, OUTPUT_DIR, PREFIX, njobs, cat_dir, ind_list=None, low=0, high=None, suffix='', starlet_thresh=0.5):
+    print('SET ENVIRONMENT')
+    kz.utils.set_env(project='HSC', name='LSBG', data_dir=DATADIR)
+    print("CURRENT WORKING DIRECTORY:", os.getcwd())
+
+    print('Number of processor to use:', njobs)
+    lsbg_cat = Table.read(cat_dir)
+    lsbg_cat.sort('viz-id')
+
+    fail_logger = kz.utils.set_logger(
+        logger_name=f'{PREFIX.lower()}_fail' + suffix, file_name=f'{PREFIX.lower()}__fail' + suffix, level='ERROR')
+    global_logger = kz.utils.set_logger(
+        logger_name=f'{PREFIX.lower()}_sample' + suffix, file_name=f'{PREFIX.lower()}__log' + suffix, level='INFO')
+
+    pool = Pool(njobs)
+
+    if high is None:
+        high = len(lsbg_cat)
+
+    if ind_list is not None:
+        iterable = ind_list
+    else:
+        iterable = np.arange(low, high, 1)
+
+    pool.map(partial(run_scarlet_vanilla, DATADIR=DATADIR, OUTPUT_DIR=OUTPUT_DIR, PREFIX=PREFIX,
+                     cat=lsbg_cat, starlet_thresh=starlet_thresh,
+                     global_logger=global_logger, fail_logger=fail_logger), iterable)
+    pool.close()
+    pool.join()
+
+
+if __name__ == '__main__':
+    fire.Fire(multiprocess_fitting)
