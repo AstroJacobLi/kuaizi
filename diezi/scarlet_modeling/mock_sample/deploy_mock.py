47c5b5fa7433f2a85b386e6450b83d9212687d75
Jiaxuan Li <jiaxuanl@princeton.edu>
2022-02-21 01:53:18 -0500

    let it run

--- diezi/scarlet_modeling/mock_sample/deploy_mock.py
+++ diezi/scarlet_modeling/mock_sample/deploy_mock.py
@@ -6,21 +6,22 @@ import sys
 import fire
 
 
-def deploy_modeling_job(low=0, high=1000, name='mock_wvlt', ncpu=32,
-                        method='wavelet', starlet_thresh=1, monotonic=True,
-                        variance=0.05**2, scales=[0, 1, 2, 3, 4, 5, 6], sigma=0.02):
+def deploy_modeling_job(low=0, high=1000, ind_list=None, name='mock_wvlt', ncpu=32,
+                        method='wavelet', starlet_thresh=0.5, monotonic=True, bkg=True, min_grad=-0.001,
+                        variance=0.03**2, scales=[0, 1, 2, 3, 4, 5, 6], sigma=0.02):
     ''' create slurm script and then submit 
     '''
     time = "48:00:00"
 
     cntnt = '\n'.join([
         "#!/bin/bash",
-        f"#SBATCH -J {name}_{low}_{high}",
+        f"#SBATCH -J {name}_{low}_{high}" if ind_list is None else f"#SBATCH -J {name}_ind_list",
         "#SBATCH --nodes=1",
         "#SBATCH --ntasks-per-node=%i" % ncpu,
+        "#SBATCH --mem-per-cpu=10G",
         "#SBATCH --time=%s" % time,
         "#SBATCH --export=ALL",
-        f"#SBATCH -o {name}_{low}_{high}.o",
+        f"#SBATCH -o {name}_{low}_{high}.o" if ind_list is None else f"#SBATCH -o {name}_ind_list.o",
         "#SBATCH --mail-type=all",
         "#SBATCH --mail-user=jiaxuanl@princeton.edu",
         "",
@@ -37,9 +38,9 @@ def deploy_modeling_job(low=0, high=1000, name='mock_wvlt', ncpu=32,
         "   --OUTPUT_SUBDIR 'MOCK_SAMPLE' \\",
         "   --PREFIX 'MOCK' \\",
         "   --cat_dir '/scratch/gpfs/jiaxuanl/Data/HSC/LSBG/Catalog/mock_sample/mock_obj_cat_0_2000.fits' \\",
-        f"   --low {low} --high {high} \\",
+        f"   --low {low} --high {high} \\" if ind_list is None else f"   --ind_list {ind_list} \\",
         f"   --suffix '' --starlet_thresh {starlet_thresh} --method {method}",
-        f"   --monotonic {monotonic} --variance {variance} --scales '{scales}'",
+        f"   --monotonic {monotonic} --bkg {bkg} --variance {variance} --scales '{scales}' --min_grad {min_grad}",
         "",
         f"python3 ../script/sclt_meas_mp.py \\",
         "    --DATADIR '/scratch/gpfs/jiaxuanl/Data/' \\",
@@ -48,7 +49,7 @@ def deploy_modeling_job(low=0, high=1000, name='mock_wvlt', ncpu=32,
         "    --PREFIX 'MOCK' \\",
         "    --cat_dir '/scratch/gpfs/jiaxuanl/Data/HSC/LSBG/Catalog/mock_sample/mock_obj_cat_0_2000.fits' \\",
         f"    --filename '_lsbg_meas_{method}_monotonic.fits' \\",
-        f"    --low {low} --high {high} --method {method}\\",
+        f"    --low {low} --high {high} --method {method}\\" if ind_list is None else f"    --ind_list {ind_list} \\",
         f"    --ncpu={ncpu} --suffix '' --sigma={sigma}",
         "",
         "",
@@ -57,10 +58,12 @@ def deploy_modeling_job(low=0, high=1000, name='mock_wvlt', ncpu=32,
         ""])
 
     # create the slurm script execute it and remove it
-    f = open(f'_{name}_{low}_{high}.slurm', 'w')
+    f = open(f'_{name}_{low}_{high}.slurm', 'w') if ind_list is None else open(
+        f'_{name}_ind_list.slurm', 'w')
     f.write(cntnt)
     f.close()
-    os.system(f'sbatch _{name}_{low}_{high}.slurm')
+    os.system(f'sbatch _{name}_{low}_{high}.slurm') if ind_list is None else os.system(
+        f'sbatch _{name}_ind_list.slurm')
     #os.system('rm _train.slurm')
     return None
 
@@ -68,6 +71,6 @@ def deploy_modeling_job(low=0, high=1000, name='mock_wvlt', ncpu=32,
 if __name__ == '__main__':
     fire.Fire(deploy_modeling_job)
 
-# python deploy_mock.py --name mock_wvlt --ncpu=16 --starlet_thresh=1 \
-#  --method=wavelet --low=0 --high=500 --monotonic=True --variance=0.05**2 \
-#  --scales="[0, 1, 2, 3, 4, 5, 6]" --sigma=0.02
+# python deploy_mock.py --name mock_wvlt --ncpu=16 --starlet_thresh=0.5 \
+#  --method=wavelet --low=0 --high=500 --monotonic=True --variance=0 --min_grad 0.01 \
+#  --scales="[0, 1, 2, 3]" --sigma=0.02
