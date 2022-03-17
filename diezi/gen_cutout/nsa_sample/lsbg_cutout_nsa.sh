1adbb2b4a4b0356ac6e3056adfd93e2c45262230
Jiaxuan Li <jiaxuanl@princeton.edu>
2022-03-17 17:28:22 -0400

    download cutouts for z001_002 sample

--- diezi/gen_cutout/nsa_sample/lsbg_cutout_nsa.sh
+++ diezi/gen_cutout/nsa_sample/lsbg_cutout_nsa.sh
@@ -7,11 +7,20 @@ export LSBGDIR="/scratch/gpfs/jiaxuanl/Data/HSC/LSBG"
 
 # everything should be downloaded to /scratch/gpfs/jiaxuanl/Data/HSC/LSBG/Cutout
 # cat['radius'] is always the cutout size we should use!!!!
+# python3 ../s18a_batch_cutout.py \
+#     $DATADIR\
+#     $LSBGDIR"/Catalog/NSA/z002_004/lsbg_NSA_MW_z002_004.fits" \
+#     --bands grizy --ra_name ra --dec_name dec \
+#     --name "viz-id" --output $LSBGDIR"/Cutout/NSA" \
+#     --catalog_dir $LSBGDIR"/Catalog/NSA/z002_004" --catalog_suffix "z002_004" \
+#     --size "cutout_size" --prefix "nsa" \
+#     --njobs 30 --psf
+
 python3 ../s18a_batch_cutout.py \
     $DATADIR\
-    $LSBGDIR"/Catalog/NSA/z002_004/lsbg_NSA_MW_z002_004.fits" \
+    $LSBGDIR"/Catalog/NSA/z001_002/lsbg_NSA_MW_z001_002.fits" \
     --bands grizy --ra_name ra --dec_name dec \
-    --name "viz-id" --output $LSBGDIR"/Cutout/NSA" \
-    --catalog_dir $LSBGDIR"/Catalog/NSA/z002_004" --catalog_suffix "z002_004" \
-    --size "cutout_size" --prefix "nsa" \
+    --name "viz-id" --output $LSBGDIR"/Cutout/NSA/z001_002" \
+    --catalog_dir $LSBGDIR"/Catalog/NSA/z001_002" --catalog_suffix "z001_002" \
+    --size 1.0 --prefix "nsa" \
     --njobs 30 --psf
