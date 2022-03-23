af4f33892804c0eda30617ffa479312f2022d623
Jiaxuan Li <jiaxuanl@princeton.edu>
2022-03-23 13:51:37 -0400

    run for 001_002 sample

--- diezi/scarlet_modeling/nsa_z001_002/deploy_nsa.py
+++ diezi/scarlet_modeling/nsa_z001_002/deploy_nsa.py
@@ -8,11 +8,29 @@ import fire
 
 def deploy_modeling_job(low=0, high=1000, ind_list=None, name='nsa_z001_002', ncpu=32,
                         method='wavelet', starlet_thresh=0.5, monotonic=True, bkg=True, min_grad=-0.001,
-                        variance=0.03**2, scales=[0, 1, 2, 3, 4, 5, 6], sigma=0.02):
+                        variance=0.03**2, scales=[0, 1, 2, 3, 4, 5, 6], sigma=0.05, only_measure=False):
     ''' create slurm script and then submit 
     '''
     time = "11:59:00"
 
+    run_scarlet_content = '\n'.join([f"python3 ../script/sclt_multiprocess.py --njobs {ncpu} \\",
+                                     "   --DATADIR '/scratch/gpfs/jiaxuanl/Data/' \\",
+                                     "   --OUTPUT_DIR '/scratch/gpfs/jiaxuanl/Data/HSC/LSBG/' \\",
+                                     f"   --OUTPUT_SUBDIR 'NSA_z001_002/{method}' \\",
+                                     "   --PREFIX 'NSA' \\",
+                                     "   --cat_dir '/scratch/gpfs/jiaxuanl/Data/HSC/LSBG/Catalog/NSA/z001_002/nsa_cutout_cat_z001_002.fits' \\",
+                                     f"   --low {low} --high {high} \\" if ind_list is None else f"   --ind_list {ind_list} \\",
+                                     f"   --suffix '' --starlet_thresh {starlet_thresh} --method {method} \\",
+                                     f"   --monotonic {monotonic} --bkg {bkg} --variance {variance} --scales '{scales}' --min_grad {min_grad}"])
+    measure_content = '\n'.join([f"python3 ../script/sclt_meas_mp.py \\",
+                                 "    --DATADIR '/scratch/gpfs/jiaxuanl/Data/' \\",
+                                 "    --OUTPUT_DIR '/scratch/gpfs/jiaxuanl/Data/HSC/LSBG/' \\",
+                                 f"    --OUTPUT_SUBDIR 'NSA_z001_002/{method}' \\",
+                                 "    --PREFIX 'NSA' \\",
+                                 "    --cat_dir '/scratch/gpfs/jiaxuanl/Data/HSC/LSBG/Catalog/NSA/z001_002/nsa_cutout_cat_z001_002.fits' \\",
+                                 f"    --filename '_lsbg_meas_{method}.fits' \\",
+                                 f"    --low {low} --high {high} --method {method}\\" if ind_list is None else f"    --ind_list {ind_list} \\",
+                                 f"    --ncpu={ncpu} --suffix '' --sigma={sigma}"])
     cntnt = '\n'.join([
         "#!/bin/bash",
         f"#SBATCH -J {name}_{low}_{high}" if ind_list is None else f"#SBATCH -J {name}_ind_list",
@@ -32,25 +50,9 @@ def deploy_modeling_job(low=0, high=1000, ind_list=None, name='nsa_z001_002', nc
         ". /home/jiaxuanl/Research/Packages/kuaizi/diezi/setup_env.sh",
         "export OMP_NUM_THREADS=1",
         "",
-        f"python3 ../script/sclt_multiprocess.py --njobs {ncpu} \\",
-        "   --DATADIR '/scratch/gpfs/jiaxuanl/Data/' \\",
-        "   --OUTPUT_DIR '/scratch/gpfs/jiaxuanl/Data/HSC/LSBG/' \\",
-        f"   --OUTPUT_SUBDIR 'NSA_z001_002/{method}' \\",
-        "   --PREFIX 'NSA' \\",
-        "   --cat_dir '/scratch/gpfs/jiaxuanl/Data/HSC/LSBG/Catalog/NSA/z001_002/nsa_cutout_cat_z001_002.fits' \\",
-        f"   --low {low} --high {high} \\" if ind_list is None else f"   --ind_list {ind_list} \\",
-        f"   --suffix '' --starlet_thresh {starlet_thresh} --method {method} \\",
-        f"   --monotonic {monotonic} --bkg {bkg} --variance {variance} --scales '{scales}' --min_grad {min_grad}",
+        run_scarlet_content if not only_measure else "",
         "",
-        f"python3 ../script/sclt_meas_mp.py \\",
-        "    --DATADIR '/scratch/gpfs/jiaxuanl/Data/' \\",
-        "    --OUTPUT_DIR '/scratch/gpfs/jiaxuanl/Data/HSC/LSBG/' \\",
-        f"    --OUTPUT_SUBDIR 'NSA_z001_002/{method}' \\",
-        "    --PREFIX 'NSA' \\",
-        "    --cat_dir '/scratch/gpfs/jiaxuanl/Data/HSC/LSBG/Catalog/NSA/z001_002/nsa_cutout_cat_z001_002.fits' \\",
-        f"    --filename '_lsbg_meas_{method}.fits' \\",
-        f"    --low {low} --high {high} --method {method}\\" if ind_list is None else f"    --ind_list {ind_list} \\",
-        f"    --ncpu={ncpu} --suffix '' --sigma={sigma}",
+        measure_content,
         "",
         "",
         'now=$(date +"%T")',
@@ -76,25 +78,28 @@ if __name__ == '__main__':
 
 # python deploy_mock.py --name mock_wvlt --ncpu=16 --starlet_thresh=0.5 \
 #  --method=wavelet --low=0 --high=500 --monotonic=True --variance=0 --min_grad 0.01 \
-#  --scales="[0, 1, 2, 3]" --sigma=0.02
+#  --scales="[0, 1, 2, 3]" --sigma=0.05
 
 ############ VANILLA #############
 # python deploy_nsa.py --name nsa --ncpu=10 --method=vanilla \
-# --low=0 --high=1000 --monotonic=True --bkg=True -min_grad=-0.02 --sigma=0.02
+# --low=0 --high=1000 --monotonic=True --bkg=True -min_grad=-0.02 --sigma=0.05 --only_measure=True
 
 # python deploy_nsa.py --name nsa --ncpu=10 --method=vanilla \
-# --low=1000 --high=2000 --monotonic=True --bkg=True -min_grad=-0.02 --sigma=0.02
+# --low=1000 --high=2000 --monotonic=True --bkg=True -min_grad=-0.02 --sigma=0.05 --only_measure=True
 
 # python deploy_nsa.py --name nsa --ncpu=10 --method=vanilla \
-# --low=2000 --high=None --monotonic=True --bkg=True -min_grad=-0.02 --sigma=0.02
+# --low=2000 --high=None --monotonic=True --bkg=True -min_grad=-0.02 --sigma=0.05 --only_measure=True
 
 
 ############ SPERGEL #############
 # python deploy_nsa.py --name nsa_spgl --ncpu=16 --method=spergel \
-# --low=0 --high=1000 --monotonic=True --bkg=True -min_grad=-0.1 --sigma=0.02
+# --low=0 --high=1000 --monotonic=True --bkg=True -min_grad=-0.1 --sigma=0.05
 
 # python deploy_nsa.py --name nsa_spgl --ncpu=16 --method=spergel \
-# --low=1000 --high=2000 --monotonic=True --bkg=True -min_grad=-0.1 --sigma=0.02
+# --low=1000 --high=2000 --monotonic=True --bkg=True -min_grad=-0.1 --sigma=0.05
 
 # python deploy_nsa.py --name nsa_spgl --ncpu=16 --method=spergel \
-# --low=2000 --high=None --monotonic=True --bkg=True -min_grad=-0.1 --sigma=0.02
+# --low=2000 --high=None --monotonic=True --bkg=True -min_grad=-0.1 --sigma=0.05
+
+# python deploy_nsa.py --name nsa_spgl --ncpu=16 --method=spergel \
+# --monotonic=True --bkg=True -min_grad=-0.1 --sigma=0.05 --ind_list='/scratch/gpfs/jiaxuanl/Data/HSC/LSBG/z001_002_failed.npy'
